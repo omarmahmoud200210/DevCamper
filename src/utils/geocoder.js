ff38e3c22aff0afde421d49ded581ba162a7fa7c
Omar Mahmoud <omar.mahmoud200210@gmail.com>
2025-12-17 22:07:17 +0200

    location

--- src/utils/geocoder.js
+++ src/utils/geocoder.js
@@ -5,7 +5,8 @@ const options = {
   httpAdapter: "https",
   apiKey: "",
   formatter: null,
-  email: "omar.mahmoud200210@gmail.com"
+  email: "omar.mahmoud200210@gmail.com",
+  userAgent: "DevCamper/1.0",
 };
 
 const geo = geocoder(options);
