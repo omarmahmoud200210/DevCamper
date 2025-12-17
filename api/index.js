3a3af5d528a8eeb617358687595f6e980a8d4d7a
Omar Mahmoud <omar.mahmoud200210@gmail.com>
2025-12-17 19:19:57 +0200

    deployment

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import app from "../src/server.js";
+import { connectWithMongoDB } from "../src/config/mongo.js";
 
 export default async (req, res) => {
+  await connectWithMongoDB(process.env.MONGO_URL);
   return app(req, res);
 };
